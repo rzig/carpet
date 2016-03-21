f7a4bcd61c415aace7dac383e227a971caa79fdd
railsrocks <rzig408@gmail.com>
2016-03-21 17:08:13 -0400

    Code fixed. Time to update the generator to match it.

--- lib/carpet/redcarpet_field.rb
+++ lib/carpet/redcarpet_field.rb
@@ -1,7 +1,7 @@
 require 'redcarpet'
 ## The module for the Carpet gem.
 module Carpet
-  ## The module for fields to be parsd.
+  ## The module for fields to be rendered.
   module RedcarpetField
     extend ActiveSupport::Concern
     ## The methods that are found in the model.
@@ -9,30 +9,30 @@ module Carpet
       def redcarpet(*fields, render_opts: {}, renderer: :default)
         fields.each do |field|
           define_method "rendered_#{field}" do
-            # This is the method seen in the model. So it maight be parsed_name
+            # This is the method seen in the model. So it might be rendered_name
             # or something.
-            Carpet::Parsing.parse(read_attribute(field), render_opts: render_opts, parser: renderer).html_safe
+            Carpet::Rendering.render(read_attribute(field), render_opts: render_opts, md_renderer: renderer).html_safe
           end # End defining the method dynamically.
         end # End the fields loop.
       end # End the redcarpet method.
     end # End the class methods module.
   end # End the redcarpetfield module.
-  module Parsing
-    def self.parse(text, render_opts: {}, parser: :default)
+  module Rendering
+    def self.render(text, render_opts: {}, md_renderer: :default)
       renderer = Redcarpet::Render::HTML.new(render_opts)
-      if parser != :default
-        renderer = load_parser(parser)
+      if md_renderer != :default
+        renderer = load_renderer(renderer, render_opts)
       end
       markdown = Redcarpet::Markdown.new(renderer)
       markdown.render text
-    end # End the parse method.
-    def self.load_parser(parser_name)
-      parser_path = Rails.root.join('mdrenderers', "#{parser_name.to_s}.rb")
-      load parser_path
-      parser = eval("Redcarpet::Render::HTML::#{parser_name.to_s.camelize}.new")
-      return parser
+    end # End the render method.
+    def self.load_renderer(renderer_name, render_opts: {})
+      renderer_path = Rails.root.join('app', 'redcarpet_renderers', "#{renderer_name.to_s}.rb")
+      load renderer_path
+      renderer = eval("#{renderer_name.to_s.camelize}.new")
+      return renderer
     end
-  end # End the parsing module.
+  end # End the Rendering module.
 end # End the carpet module.
 
 ActiveRecord::Base.send :include, Carpet::RedcarpetField
