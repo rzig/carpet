9f369a3b1860a12fc2ee5f2e2cf499d3350574d6
railsrocks <rzig408@gmail.com>
2016-03-28 09:49:14 -0400

    Fixed renderer_opts code.

--- lib/carpet/redcarpet_field.rb
+++ lib/carpet/redcarpet_field.rb
@@ -16,12 +16,12 @@ module Carpet
             if as[0]
               as.each do |method_name|
                 define_method "#{method_name}" do
-                Carpet::Rendering.render(read_attribute(field), render_opts: render_opts, rc_renderer: renderer).html_safe
+                Carpet::Rendering.render(read_attribute(field), renderer_opts: render_opts, rc_renderer: renderer).html_safe
                 end # End defining the method dynamically.
               end
             else
               define_method "rendered_#{field}" do
-              Carpet::Rendering.render(read_attribute(field), render_opts: render_opts, rc_renderer: renderer).html_safe
+              Carpet::Rendering.render(read_attribute(field), renderer_opts: render_opts, rc_renderer: renderer).html_safe
               end # End defining the method dynamically.
             end
           end
@@ -31,17 +31,17 @@ module Carpet
   end # End the redcarpetfield module.
   module Rendering
     def self.render(text, renderer_opts: {}, rc_renderer: :default)
-      renderer = Redcarpet::Render::HTML.new(render_opts)
+      renderer = Redcarpet::Render::HTML.new(renderer_opts)
       if rc_renderer != :default
-        renderer = load_renderer(rc_renderer, render_opts)
+        renderer = load_renderer(rc_renderer, renderer_opts)
       end
       markdown = Redcarpet::Markdown.new(renderer)
       markdown.render text
     end # End the render method.
-    def self.load_renderer(renderer_name, render_opts: {})
+    def self.load_renderer(renderer_name, renderer_opts)
       renderer_path = Rails.root.join('app', 'redcarpet_renderers', "#{renderer_name.to_s}.rb")
       load renderer_path
-      renderer = eval("#{renderer_name.to_s.camelize}.new")
+      renderer = eval("#{renderer_name.to_s.camelize}.new(renderer_opts)")
       return renderer
     end
   end # End the Rendering module.
--- test/dummy/app/models/person.rb
+++ test/dummy/app/models/person.rb
@@ -1,5 +1,4 @@
 class Person < ApplicationRecord
   redcarpetable :about, render_opts: {no_links: true}
-  redcarpetable :name, renderer: :simple_parser, as: [:cool_name, :rendered_name], render_opts: {:generate_toc_data: true
-  }
+  redcarpetable :name, renderer: :simple_parser, as: [:cool_name, :rendered_name], render_opts: {no_links: true}
 end
